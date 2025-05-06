d9fc66864874c1aa897171a766cccb15d368b10a
xianzhu.zhang <xianzhu.zhang@t2mobile.com>
2025-05-06 17:46:25 +0800

    [FPS-2034][2/2] Battery Protect does not work .
    *Comment
    Battery Protect does not work.
    *Root Cause
    normally battery SOC above 80% will vote USBIN current to 0 and charing mode switch to SCP_INOV,but in this mode will
    override the vote ICL value icm_lib_rtn_set(tree, ICL_AGG_EXT, current_ma); in the function scp_inov_conf_for_adpt ,
    so the device will enter charing state again .
    *Solution
    not vote USBIN current , vote FCC to 0 instead, and resolve the charging re-enable issue when FCC vote is 0.
    *Test Steps
    battery protect feature ,dut not charge after battery level above 80%.
    *Test Result
    Pass
    
    Change-Id: Ic256118a92152af874c5b91effa919deeb705784

--- drivers/power/supply/qti_battery_charger.c
+++ drivers/power/supply/qti_battery_charger.c
@@ -370,6 +370,10 @@ static const char * const qc_power_supply_wls_type_text[] = {
 /*FPS-184 ,set usb online to false when lpd is ture*/
 static int moisture_detected = 0;
 
+
+static u32 chg_dis_global = 0;
+
+
 static RAW_NOTIFIER_HEAD(hboost_notifier);
 
 int register_hboost_event_notifier(struct notifier_block *nb)
@@ -1173,9 +1177,9 @@ static int usb_psy_get_prop(struct power_supply *psy,
 
 /*FPS-184 ,set usb online to false when lpd is ture ,begin */
 
-	if ((prop == POWER_SUPPLY_PROP_ONLINE) && (1 == moisture_detected)){
+	if ((prop == POWER_SUPPLY_PROP_ONLINE) && ((1 == moisture_detected)||(1 == chg_dis_global))){
 		pval->intval = 0;
-		pr_err("usb_psy_get_prop,set online = 0 when moisture_detected ! \n");
+		//pr_info("usb_psy_get_prop,set online = 0 when moisture_detected=%d,chg_dis_global=%d ! \n",moisture_detected,chg_dis_global);
 	}
 
 	return 0;
@@ -2082,7 +2086,7 @@ static ssize_t suspend_input_current_store(struct class *c, struct class_attribu
 	if (kstrtoint(buf, 0, &val))
 		return -EINVAL;
 
-	pr_debug("suspend input current %d\n", val);
+	pr_info("suspend input current %d\n", val);
 
 	rc = write_property_id(bcdev, pst, USB_SUSPEND_INPUT_CURRENT, val);
 	if (rc < 0)
@@ -2249,6 +2253,9 @@ static ssize_t charge_disable_store(struct class *c, struct class_attribute *att
 	rc = write_property_id(bcdev, pst, BATT_CHG_DISABLE_CHARGING, chg_dis);
 	if (rc < 0)
 		return rc;
+
+	chg_dis_global = chg_dis ;
+
 	return count;
 }
 
