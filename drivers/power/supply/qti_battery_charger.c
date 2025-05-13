90dee1eb0fdb2c49b698da1c9218cc1eb3c7ece2
xianzhu.zhang <xianzhu.zhang@t2mobile.com>
2025-05-13 18:23:06 +0800

    [FPS-2638][2/2]change max charge current to 5800mA.
    *Comment
    change max charge current to 5800mA.
    *Root Cause
    from HW requirement change max charge current to 5800mA  , for sumsung adapter charge current more than 6A .
    *Solution
    change max charge current to 5800mA.
    *Test Steps
    checktsumsung adapter charge current.
    *Test Result
    Pass
    
    Change-Id: Ia9844f043d961178286c9e8ab3c3f8ce697e716f

--- drivers/power/supply/qti_battery_charger.c
+++ drivers/power/supply/qti_battery_charger.c
@@ -2305,7 +2305,7 @@ static CLASS_ATTR_RO(typec_cc_orientation);
 #ifdef CHARGE_MODE_FCC_SUPPORT
 
 #define SLOW_MODE_FCC 1000000 // 1A
-#define NORMAL_MODE_FCC 6000000 // 6A
+#define NORMAL_MODE_FCC 5800000 // 5.8A
 
 static ssize_t chgmod_fcc_store(struct class *c, struct class_attribute *attr,
 				const char *buf, size_t count)
@@ -2319,7 +2319,7 @@ static ssize_t chgmod_fcc_store(struct class *c, struct class_attribute *attr,
 	if (kstrtoint(buf, 0, &val))
 		return -EINVAL;
 
-	#if 1 //need add check val later
+	#if 0 //need add check val later
 	if(val > 0)
 		fcc_ua = val ;
 	#else
@@ -2356,7 +2356,7 @@ static CLASS_ATTR_RW(chgmod_fcc);
 
 //FPS-2299
 #define DISPLAY_ON_FCC 4000000 // 4A
-#define DISPLAY_OFF_FCC 6000000 // 6A
+#define DISPLAY_OFF_FCC 5800000 // 5.8A
 
 static ssize_t display_fcc_store(struct class *c, struct class_attribute *attr,
 				const char *buf, size_t count)
@@ -2370,7 +2370,7 @@ static ssize_t display_fcc_store(struct class *c, struct class_attribute *attr,
 	if (kstrtoint(buf, 0, &val))
 		return -EINVAL;
 
-	#if 1 //set by framework
+	#if 0 //set by framework
 	if(val > 0)
 		fcc_ua = val ;
 	#else
