4e8908a882f26c1ea9452a8b6916dc7afc3d529d
Tim Zimmermann <tim@linux4.de>
2025-10-05 09:38:28 +0200

    scripts/dtc: Update to upstream version v1.7.0
    This adds the following commits from upstream:
    
    039a994 Bump version to v1.7.0
    9b62ec8 Merge remote-tracking branch 'gitlab/main'
    3f29d6d pylibfdt: add size_hint parameter for get_path
    2022bb1 checks: Update #{size,address}-cells check for 'dma-ranges'
    abbd523 pylibfdt: Work-around SWIG limitations with flexible arrays
    a41509b libfdt: Replace deprecated 0-length arrays with proper flexible arrays
    2cd89f8 dtc: Warning rather than error on possible truncation of cell values
    55778a0 libfdt: tests: add get_next_tag_invalid_prop_len
    7359034 libfdt: prevent integer overflow in fdt_next_tag
    035fb90 libfdt: add fdt_get_property_by_offset_w helper
    98a0700 Makefile: fix infinite recursion by dropping non-existent `%.output`
    a036cc7 Makefile: limit make re-execution to avoid infinite spin
    c6e9210 libdtc: remove duplicate judgments
    e37c256 Don't generate erroneous fixups from reference to path
    5045465 libfdt: Don't mask fdt_get_name() returned error
    e64a204 manual.txt: Follow README.md and remove Jon
    f508c83 Update README in MANIFEST.in and setup.py to README.md
    c2ccf8a Add description of Signed-off-by lines
    90b9d9d Split out information for contributors to CONTRIBUTING.md
    0ee1d47 Remove Jon Loeliger from maintainers list
    b33a73c Convert README to README.md
    7ad6073 Allow static building with meson
    fd9b8c9 Allow static building with make
    fda71da libfdt: Handle failed get_name() on BEGIN_NODE
    c7c7f17 Fix test script to run also on dash shell
    01f23ff Add missing relref_merge test to meson test list
    ed31080 pylibfdt: add FdtRo.get_path()
    c001fc0 pylibfdt: fix swig build in install
    26c54f8 tests: add test cases for label-relative path references
    ec7986e dtc: introduce label relative path references
    651410e util: introduce xstrndup helper
    4048aed setup.py: fix out of tree build
    ff5afb9 Handle integer overflow in check_property_phandle_args()
    ca72944 README: Explain how to add a new API function
    c0c2e11 Fix a UB when fdt_get_string return null
    cd5f69c tests: setprop_inplace: use xstrdup instead of unchecked strdup
    a04f690 pylibfdt: add Property.as_*int*_array()
    8310271 pylibfdt: add Property.as_stringlist()
    d152126 Fix Python crash on getprop deallocation
    17739b7 Support 'r' format for printing raw bytes with fdtget
    45f3d1a libfdt: overlay: make overlay_get_target() public
    c19a4ba libfdt: fix an incorrect integer promotion
    1cc41b1 pylibfdt: Add packaging metadata
    db72398 README: Update pylibfdt install instructions
    383e148 pylibfdt: fix with Python 3.10
    23b56cb pylibfdt: Move setup.py to the top level
    69a7607 pylibfdt: Split setup.py author name and email
    0b106a7 pylibfdt: Use setuptools_scm for the version
    c691776 pylibfdt: Use setuptools instead of distutils
    5216f3f libfdt: Add static lib to meson build
    4eda259 CI: Cirrus: bump used FreeBSD from 12.1 to 13.0
    
    Change-Id: I96593e4b5d7ed2f0c5ed7ae742366791ecda1705
    Signed-off-by: Tim Zimmermann <tim@linux4.de>

--- scripts/dtc/fdtget.c
+++ scripts/dtc/fdtget.c
@@ -40,6 +40,43 @@ static void report_error(const char *where, int err)
 }
 
 /**
+ * Shows a list of cells in the requested format
+ *
+ * @param disp		Display information / options
+ * @param data		Data to display
+ * @param len		Maximum length of buffer
+ * @param size		Data size to use for display (e.g. 4 for 32-bit)
+ * @return 0 if ok, -1 on error
+ */
+static int show_cell_list(struct display_info *disp, const char *data, int len,
+			  int size)
+{
+	const uint8_t *p = (const uint8_t *)data;
+	char fmt[3];
+	int value;
+	int i;
+
+	fmt[0] = '%';
+	fmt[1] = disp->type ? disp->type : 'd';
+	fmt[2] = '\0';
+	for (i = 0; i < len; i += size, p += size) {
+		if (i)
+			printf(" ");
+		switch (size) {
+		case 4: value = fdt32_ld((const fdt32_t *)p); break;
+		case 2: value = fdt16_ld((const fdt16_t *)p); break;
+		case 1:
+		default:
+			value = *p;
+			break;
+		}
+		printf(fmt, value);
+	}
+
+	return 0;
+}
+
+/**
  * Displays data of a given length according to selected options
  *
  * If a specific data type is provided in disp, then this is used. Otherwise
@@ -52,16 +89,18 @@ static void report_error(const char *where, int err)
  */
 static int show_data(struct display_info *disp, const char *data, int len)
 {
-	int i, size;
-	const uint8_t *p = (const uint8_t *)data;
+	int size;
 	const char *s;
-	int value;
 	int is_string;
-	char fmt[3];
 
 	/* no data, don't print */
 	if (len == 0)
 		return 0;
+
+	if (disp->type == 'r') {
+		fwrite(data, 1, len, stdout);
+		return 0;
+	}
 
 	is_string = (disp->type) == 's' ||
 		(!disp->type && util_is_printable_string(data, len));
@@ -85,17 +124,8 @@ static int show_data(struct display_info *disp, const char *data, int len)
 				"selected data size\n");
 		return -1;
 	}
-	fmt[0] = '%';
-	fmt[1] = disp->type ? disp->type : 'd';
-	fmt[2] = '\0';
-	for (i = 0; i < len; i += size, p += size) {
-		if (i)
-			printf(" ");
-		value = size == 4 ? fdt32_to_cpu(*(const uint32_t *)p) :
-			size == 2 ? (*p << 8) | p[1] : *p;
-		printf(fmt, value);
-	}
-	return 0;
+
+	return show_cell_list(disp, data, len, size);
 }
 
 /**
@@ -107,7 +137,6 @@ static int show_data(struct display_info *disp, const char *data, int len)
  */
 static int list_properties(const void *blob, int node)
 {
-	const struct fdt_property *data;
 	const char *name;
 	int prop;
 
@@ -116,8 +145,7 @@ static int list_properties(const void *blob, int node)
 		/* Stop silently when there are no more properties */
 		if (prop < 0)
 			return prop == -FDT_ERR_NOTFOUND ? 0 : prop;
-		data = fdt_get_property_by_offset(blob, prop, NULL);
-		name = fdt_string(blob, fdt32_to_cpu(data->nameoff));
+		fdt_getprop_by_offset(blob, prop, &name, NULL);
 		if (name)
 			puts(name);
 		prop = fdt_next_property_offset(blob, prop);
@@ -231,7 +259,7 @@ static int show_data_for_item(const void *blob, struct display_info *disp,
  * @param filename	Filename of blob file
  * @param arg		List of arguments to process
  * @param arg_count	Number of arguments
- * @param return 0 if ok, -ve on error
+ * @return 0 if ok, -ve on error
  */
 static int do_fdtget(struct display_info *disp, const char *filename,
 		     char **arg, int arg_count, int args_per_step)
@@ -240,7 +268,7 @@ static int do_fdtget(struct display_info *disp, const char *filename,
 	const char *prop;
 	int i, node;
 
-	blob = utilfdt_read(filename);
+	blob = utilfdt_read(filename, NULL);
 	if (!blob)
 		return -1;
 
@@ -252,44 +280,50 @@ static int do_fdtget(struct display_info *disp, const char *filename,
 				continue;
 			} else {
 				report_error(arg[i], node);
+				free(blob);
 				return -1;
 			}
 		}
 		prop = args_per_step == 1 ? NULL : arg[i + 1];
 
-		if (show_data_for_item(blob, disp, node, prop))
+		if (show_data_for_item(blob, disp, node, prop)) {
+			free(blob);
 			return -1;
+		}
 	}
+
+	free(blob);
+
 	return 0;
 }
 
-static const char *usage_msg =
-	"fdtget - read values from device tree\n"
-	"\n"
-	"Each value is printed on a new line.\n\n"
-	"Usage:\n"
+/* Usage related data. */
+static const char usage_synopsis[] =
+	"read values from device tree\n"
 	"	fdtget <options> <dt file> [<node> <property>]...\n"
 	"	fdtget -p <options> <dt file> [<node> ]...\n"
-	"Options:\n"
-	"\t-t <type>\tType of data\n"
-	"\t-p\t\tList properties for each node\n"
-	"\t-l\t\tList subnodes for each node\n"
-	"\t-d\t\tDefault value to display when the property is "
-			"missing\n"
-	"\t-h\t\tPrint this help\n\n"
+	"\n"
+	"Each value is printed on a new line.\n"
 	USAGE_TYPE_MSG;
-
-static void usage(const char *msg)
-{
-	if (msg)
-		fprintf(stderr, "Error: %s\n\n", msg);
-
-	fprintf(stderr, "%s", usage_msg);
-	exit(2);
-}
+static const char usage_short_opts[] = "t:pld:" USAGE_COMMON_SHORT_OPTS;
+static struct option const usage_long_opts[] = {
+	{"type",              a_argument, NULL, 't'},
+	{"properties",       no_argument, NULL, 'p'},
+	{"list",             no_argument, NULL, 'l'},
+	{"default",           a_argument, NULL, 'd'},
+	USAGE_COMMON_LONG_OPTS,
+};
+static const char * const usage_opts_help[] = {
+	"Type of data",
+	"List properties for each node",
+	"List subnodes for each node",
+	"Default value to display when the property is missing",
+	USAGE_COMMON_OPTS_HELP
+};
 
 int main(int argc, char *argv[])
 {
+	int opt;
 	char *filename = NULL;
 	struct display_info disp;
 	int args_per_step = 2;
@@ -298,20 +332,14 @@ int main(int argc, char *argv[])
 	memset(&disp, '\0', sizeof(disp));
 	disp.size = -1;
 	disp.mode = MODE_SHOW_VALUE;
-	for (;;) {
-		int c = getopt(argc, argv, "d:hlpt:");
-		if (c == -1)
-			break;
-
-		switch (c) {
-		case 'h':
-		case '?':
-			usage(NULL);
+	while ((opt = util_getopt_long()) != EOF) {
+		switch (opt) {
+		case_USAGE_COMMON_FLAGS
 
 		case 't':
 			if (utilfdt_decode_type(optarg, &disp.type,
 					&disp.size))
-				usage("Invalid type string");
+				usage("invalid type string");
 			break;
 
 		case 'p':
@@ -333,7 +361,7 @@ int main(int argc, char *argv[])
 	if (optind < argc)
 		filename = argv[optind++];
 	if (!filename)
-		usage("Missing filename");
+		usage("missing filename");
 
 	argv += optind;
 	argc -= optind;
@@ -344,7 +372,7 @@ int main(int argc, char *argv[])
 
 	/* Check for node, property arguments */
 	if (args_per_step == 2 && (argc % 2))
-		usage("Must have an even number of arguments");
+		usage("must have an even number of arguments");
 
 	if (do_fdtget(&disp, filename, argv, argc, args_per_step))
 		return 1;
